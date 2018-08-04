d1de528ef1211bb76a87b27a9bb7bf727a6fd6f5
Ben Halpern <ben@dev.to>
2018-08-04 14:07:36 -0400

    Create basic app

---
+++ DEV/ConnectViewController.swift
@@ -0,0 +1,57 @@
+import Foundation
+import UIKit
+import WebKit
+class ConnectViewController: UIViewController, WKNavigationDelegate {
+    @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var leftButton: UIBarButtonItem!
+    @IBOutlet weak var Activity: UIActivityIndicatorView!
+
+    @IBAction func buttonTapped(_ sender: Any) {
+        if self.webView.canGoBack {
+            self.webView.scrollView.setContentOffset(self.webView.scrollView.contentOffset, animated: false)
+            self.webView.goBack()
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        webView.navigationDelegate = self
+        webView.backForwardList.perform(Selector(("_removeAllItems")))
+        webView.addObserver(self, forKeyPath: "URL", options: [.new, .old], context: nil)
+        webView.scrollView.isScrollEnabled = false
+        if let url = URL(string: "https://dev.to/connect?rand="+MainHelper.randomString(length: 10)) {
+            webView.load(URLRequest.init(url: url))
+        }
+        
+        self.Activity.startAnimating()
+        self.Activity.hidesWhenStopped = true
+        webView.backForwardList.perform(Selector(("_removeAllItems")))
+
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        manageBackButton()
+    }
+    
+    func manageBackButton(){
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.05) { //race condition hack
+            if self.webView.canGoBack {
+                self.leftButton?.isEnabled = true
+                self.leftButton.tintColor = UIColor.black
+            } else {
+                self.leftButton?.isEnabled = false
+                self.leftButton?.tintColor = UIColor.clear
+            }
+        }
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        Activity.stopAnimating()
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        Activity.stopAnimating()
+    }
+
+    
+}
