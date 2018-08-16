7cf2d7cf6b53937a856233bc78a90ff278d413bc
Wahyu Sumartha <wahyusumartha@gmail.com>
2018-08-16 23:45:58 +0200

    add new blue convenience color

--- DEV/Extensions/UIColor+DevColor.swift
+++ DEV/Extensions/UIColor+DevColor.swift
@@ -12,4 +12,8 @@ extension UIColor {
     static var creamColor: UIColor {
         return #colorLiteral(red: 0.9882352941, green: 0.9764705882, blue: 0.9568627451, alpha: 1)
     }
+    
+    static var devBlue: UIColor {
+        return #colorLiteral(red: 0, green: 0.1725490196, blue: 0.3607843137, alpha: 1)
+    }
 }
