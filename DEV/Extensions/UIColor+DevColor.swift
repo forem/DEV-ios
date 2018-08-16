ffcf92c3a6eca0038a961826ff1a948f7181e472
Wahyu Sumartha <wahyusumartha@gmail.com>
2018-08-16 23:26:24 +0200

    add custom view controller to load an url

---
+++ DEV/Extensions/UIColor+DevColor.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+DevColor.swift
+//  DEV
+//
+//  Created by Wahyu Sumartha Priya Dharma on 16.08.18.
+//  Copyright © 2018 DEV. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var creamColor: UIColor {
+        return #colorLiteral(red: 0.9882352941, green: 0.9764705882, blue: 0.9568627451, alpha: 1)
+    }
+}
