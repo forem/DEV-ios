c8004eefa8953cc9d6e39bb0bd2fe800664644ef
Ben Halpern <ben@dev.to>
2018-11-12 08:23:13 -0500

    deleting old files before adding updated project

--- DEV/MainTabController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MainTabController.swift
-//  DEV
-//
-//  Created by Ben Halpern on 6/9/18.
-//  Copyright © 2018 DEV. All rights reserved.
-//
-
-import Foundation
--- DEV/UITabBarController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UITabBarController.swift
-//  DEV
-//
-//  Created by Ben Halpern on 7/30/18.
-//  Copyright © 2018 DEV. All rights reserved.
-//
-
-import Foundation
