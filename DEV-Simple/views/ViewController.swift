5958a5c11452f1664bcb434fa8776804ddd3a7ad
Michal Rogowski <michrogowski@gmail.com>
2019-10-05 21:02:06 +0200

    [#180] App crashes when DarkTheme enabled on iOS 13

--- DEV-Simple/views/ViewController.swift
+++ DEV-Simple/views/ViewController.swift
@@ -226,11 +226,6 @@ class ViewController: UIViewController {
     }
 
     private func applyDarkTheme() {
-        guard let statusBarView = UIApplication.shared.value(forKeyPath: "statusBarWindow.statusBar") as? UIView else {
-            return
-        }
-
-        statusBarView.backgroundColor = darkBackgroundColor
         useDarkMode = true
         setNeedsStatusBarAppearanceUpdate()
         navigationToolBar.isTranslucent = false
