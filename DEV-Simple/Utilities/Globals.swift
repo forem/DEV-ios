4db7cfbffcbfdae4ad64c8f7aeb85112279676dc
Fernando Valverde <fdov88@gmail.com>
2020-07-12 10:48:58 -0600

    missing themes

--- DEV-Simple/Utilities/Globals.swift
+++ DEV-Simple/Utilities/Globals.swift
@@ -6,10 +6,15 @@
 //  Copyright © 2019 DEV. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct DoAction {
     static var openExternalURL = "showExternalPage"
+}
+
+struct ThemeColors {
+    static var statusBarStyleDarkContentRawValue = 3
+    static var darkBackgroundColor = UIColor(red: 26/255, green: 38/255, blue: 52/255, alpha: 1)
 }
 
 struct AuthUrl {
