2b49399022e75225c1c7f024e86aac2a07942ff2
Fernando Valverde <fdov88@gmail.com>
2020-11-09 18:34:22 -0600

    Adapts tests to ForemWebView

--- DEV-SimpleTests/ViewControllerTests.swift
+++ DEV-SimpleTests/ViewControllerTests.swift
@@ -17,32 +17,22 @@ class ViewControllerTests: XCTestCase {
     override func setUp() {
         let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
         viewController = mainStoryboard.instantiateInitialViewController() as? ViewController
-        viewController.devToURL = "test.html"
         super.setUp()
     }
     
     func testCustomUserAgent() {
         _ = viewController.view
-        // Then the customerUserAgent it set correctly
-        XCTAssertEqual(viewController.webView.customUserAgent, "DEV-Native-ios")
-    }
-
-    func testPolicyForNavigationAction() {
-
-        let nonDev      = URL(string: "https://someOtherUrl")!
-        let aboutBlank  = URL(string: "about:blank")!
-        let twitterAuth = URL(string: "https://github.com/login/extra/test/stuff")!
-        let githubAuth  = URL(string: "https://api.twitter.com/oauth/extra/test/stuff")!
-        let devTo       = URL(string: "https://dev.to")!
-        let mail        = URL(string: "mailto:test@test.com")!
         
-        XCTAssertEqual(viewController.navigationPolicy(url: nonDev,      navigationType: .linkActivated), .cancel)
-        XCTAssertEqual(viewController.navigationPolicy(url: nonDev,      navigationType: .backForward),   .allow)
-        XCTAssertEqual(viewController.navigationPolicy(url: aboutBlank,  navigationType: .linkActivated), .allow)
-        XCTAssertEqual(viewController.navigationPolicy(url: twitterAuth, navigationType: .backForward),   .allow)
-        XCTAssertEqual(viewController.navigationPolicy(url: githubAuth,  navigationType: .linkActivated), .allow)
-        XCTAssertEqual(viewController.navigationPolicy(url: devTo,       navigationType: .backForward),   .allow)
-        XCTAssertEqual(viewController.navigationPolicy(url: mail,        navigationType: .linkActivated), .cancel)
-
+        let promise = expectation(description: "User Agent expectation")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            let usesForemWebViewUserAgent = self.viewController.webView.customUserAgent?.contains("ForemWebView")
+            XCTAssertTrue(usesForemWebViewUserAgent ?? false)
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 10)
+    }
+    
+    func testDevtoBaseUrl() {
+        XCTAssertEqual(self.viewController.devToURL, "https://dev.to")
     }
 }
