8c65a94248a727ba1e9bd7c70cdbbbdd18914ae9
Daniel Chick <daniel.chick@hotmail.com>
2020-11-27 19:28:11 -0600

    Remove merge artifact

--- DEV-Simple/ViewController.swift
+++ DEV-Simple/ViewController.swift
@@ -23,7 +23,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var navigationToolBar: UIToolbar!
 
-    let pushNotifications = PushNotifications.shared
     lazy var errorBanner: NotificationBanner = {
         let banner = NotificationBanner(title: "Network not reachable", style: .danger)
         banner.autoDismiss = false
